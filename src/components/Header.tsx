a2531dfee0f7b621befa287d7b6657e784167039
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-09 19:52:21 -0500

    Add: Link to home on logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
+
 function Header() {
   return (
     <header className='sticky top-0 bg-gray-800 p-4'>
       <div className='container mx-auto'>
-        <h1 className='text-2xl font-medium text-white'>Todo App</h1>
+        <Link href='/'>
+          <h1 className='text-2xl font-medium text-white'>Todo App</h1>
+        </Link>
       </div>
     </header>
   );
