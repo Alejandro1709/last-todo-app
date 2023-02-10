f91b80e0e1f0949edc965054ff20030c18e5701f
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-09 19:22:57 -0500

    Add: Layout component and Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className='sticky top-0 bg-gray-800 p-4'>
+      <div className='container mx-auto'>
+        <h1 className='text-2xl font-medium text-white'>Todo App</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
