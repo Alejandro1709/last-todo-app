69ebe9bbef275096c17e1ea108c9d15712dcb587
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-10 14:46:51 -0500

    Add: Prisma and ran migration

--- src/pages/todos/[slug].tsx
+++ src/pages/todos/[slug].tsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Container from '@/components/Container';
 import Layout from '@/components/Layout';
+import { getTodo } from '@/actions/todoActions';
 
 function TodoPage() {
+  const router = useRouter();
+
+  const { slug } = router.query;
+
+  useEffect(() => {
+    if (typeof slug !== 'string') return;
+    console.log(slug);
+    // getTodo(slug, dispatch);
+  }, []);
+
   return (
     <Layout title='Todo Title' description='Todo Description'>
       <Container>
