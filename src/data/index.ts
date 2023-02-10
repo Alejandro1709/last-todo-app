41b0dc90abcf66f60eacf64eeb1f5ca65b744d35
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-02-10 17:44:51 -0500

    Remove: Dummy todos

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,30 +1,5 @@
 import type ITodo from '@/types/todo';
 
-const todos: ITodo[] = [
-  {
-    id: 1,
-    title: 'Learn Vue 3',
-    slug: 'learn-vue-3',
-    completed: false,
-  },
-  {
-    id: 2,
-    title: 'Clean warehouse by tomorrow morning',
-    slug: 'clean-warehouse-by-tomorrow-morning',
-    completed: false,
-  },
-  {
-    id: 3,
-    title: 'Buy groceries',
-    slug: 'buy-groceries',
-    completed: false,
-  },
-  {
-    id: 4,
-    title: 'Learn TypeScript',
-    slug: 'learn-typescript',
-    completed: true,
-  },
-];
+const todos: ITodo[] = [];
 
 export default todos;
